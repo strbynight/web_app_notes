9e4de7370d7eda02064643e13f1b0dc248187282
Анастасия <pohtabi@mail.ru>
2026-06-19 15:38:57 +0300

    fix app.py

--- backend/app.py
+++ backend/app.py
@@ -57,8 +57,7 @@ def add_notes():
     
 @app.route('/api/health', methods=['GET'])
 def health_check():
-    #return jsonify({"status": "healthy"}), 200
-    return jsonify({"status": "broken-status"}), 200
+    return jsonify({"status": "healthy"}), 200
 
 # --- Запуск сервера (порт 5005 для команды 05) ---
 if __name__ == '__main__':
